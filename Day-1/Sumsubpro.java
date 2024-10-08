3a62df8a8302514ddc6c19985d3487afca804cc5
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-10-08 18:07:36 +0530

    main

--- Day-1/Sumsubpro.java
+++ Day-1/Sumsubpro.java
@@ -1,3 +1,14 @@
+import java.util.*;
+
 public class Sumsubpro {
-    
+    // Summition function
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number/Integer Value: ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second Number/Integer Value: ");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("Total Sum: " + sum);
+    }
 }
