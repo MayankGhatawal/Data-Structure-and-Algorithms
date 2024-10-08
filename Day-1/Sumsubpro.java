b23a983a0ffa28e413bd0f9636cd1ae33653500b
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-10-08 18:13:55 +0530

    main

--- Day-1/Sumsubpro.java
+++ Day-1/Sumsubpro.java
@@ -1,14 +1,18 @@
 import java.util.*;
 
 public class Sumsubpro {
-    // Summition function
+    // Summition/Substract/Product function
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter First Number/Integer Value: ");
-        int a = sc.nextInt();
+        float a = sc.nextFloat();
         System.out.print("Enter Second Number/Integer Value: ");
-        int b = sc.nextInt();
-        int sum = a + b;
+        float b = sc.nextFloat();
+        float sum = a + b;
+        float sub = a - b;
+        float pro = a * b;
         System.out.println("Total Sum: " + sum);
+        System.out.println("Total Difference: " + sub);
+        System.out.println("Total Product: " + pro);
     }
 }
