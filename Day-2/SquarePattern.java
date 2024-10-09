3f58dc818fc31734fc8367698c0d5f39412fafb7
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-10-09 09:08:32 +0530

    main

---
+++ Day-2/SquarePattern.java
@@ -0,0 +1,8 @@
+public class SquarePattern {
+    public static void main(String[] args) {
+        System.out.println("Square Pattern");
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("*****");
+        }
+    }
+}
