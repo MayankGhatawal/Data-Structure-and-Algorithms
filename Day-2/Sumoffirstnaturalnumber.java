400b3d132dc4439a236fc685809ac2ba383547b3
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-10-09 09:00:44 +0530

    main

--- Day-2/Sumoffirstnaturalnumber.java
+++ Day-2/Sumoffirstnaturalnumber.java
@@ -1,3 +1,17 @@
+import java.util.*;
+
 public class Sumoffirstnaturalnumber {
-    
+    public static void main(String[] args) {
+        System.out.println("Welcome to Sum of First Natural");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = scanner.nextInt();
+        int sum = 0;
+        int i = 1;
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+        System.out.println("Sum of First Natural is: " + sum);
+    }
 }
