ecbb601645817c614b2b0aa598943ea958c7ed5b
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-10-09 08:45:57 +0530

    main

---
+++ Day-2/Onetoten.java
@@ -0,0 +1,10 @@
+public class Onetoten{
+    public static void main(String[] args){
+        System.out.println("Print Number 1 - 10");
+        int i = 1;
+        while (i <= 10) { 
+            System.out.print(i);
+            i++;  // incrementing the value of i by 1 each time
+        }
+    }
+}
